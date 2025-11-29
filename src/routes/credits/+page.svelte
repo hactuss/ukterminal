d0a26161c71fa579a60e16eb71885ebc15fdbb21
hactuss <hactuss@proton.me>
2025-11-29 22:49:31 +0100

    modified:   src/routes/credits/+page.svelte

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,6 +1,7 @@
 <main>
 	<ul>
 		<li>Original idea: Hakita, developer of Ultrakil</li>
+		<li>Terminal information: https://ultrakill.wiki.gg/wiki/Terminals</li>
 		<li>Font: https://www.dafont.com/vcr-osd-mono.font</li>
 		<li>
 			Shoutout @lebaush and @eps1110 from the NewBlood Discord. For waiting patiently for this
