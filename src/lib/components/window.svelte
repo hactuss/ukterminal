75ada33c343e11bb15819b0f388bce806b0971eb
hactuss <157395485+hactuss@users.noreply.github.com>
2025-11-29 22:36:01 +0100

    Add files via upload

---
+++ src/lib/components/window.svelte
@@ -0,0 +1,28 @@
+<script>
+	let props = $props();
+</script>
+
+<main>
+	<nav>
+		<div class="flex">
+			<img src={props.icon} alt="WindowIcon" id="windowicon" />
+			<h1>{props.name}</h1>
+		</div>
+	</nav>
+</main>
+
+<style lang="scss">
+	main {
+		margin: 10px;
+		border: solid 5px rgb(150 0 0);
+		border-radius: 5px;
+	}
+	nav {
+		display: flex;
+		background-color: red;
+	}
+
+	#windowicon {
+		height: 100%;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Window from '$lib/components/window.svelte';
+</script>
+
+<Window name="SmileOS V2.0" icon="/src/lib/assets/images/IconSearch.png" />
