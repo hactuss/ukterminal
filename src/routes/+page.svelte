3c6a713b3579a154abc1b1559afde54ece4c1c41
Hactuss <157395485+hactuss@users.noreply.github.com>
2026-01-16 21:01:29 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,7 @@
 </script>
 
 <Window name="SmileOS V2.0" icon="/src/lib/assets/images/IconSearch.png" />
+<!-- 
+https://ultrakill.wiki.gg/images/SmileOS_2_Header.png?dd2397
+https://ultrakill.wiki.gg/images/SmileOS_2_logo_info.png
+-->
