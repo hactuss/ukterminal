f4c6e67ca932eab17a83e218ceb1f84f85db5a8b
hactuss <hactuss@proton.me>
2026-01-23 20:44:35 +0100

    new file:   .gitignore 	new file:   .npmrc 	modified:   README.md 	deleted:    deno.lock 	deleted:    eslint.config.js 	modified:   package.json 	new file:   pnpm-lock.yaml 	new file:   pnpm-workspace.yaml 	renamed:    src/app.scss -> src/app.css 	modified:   src/routes/+layout.svelte 	modified:   svelte.config.js 	modified:   tsconfig.json 	modified:   vite.config.ts 	deleted:    vitest-setup-client.ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-	import '../app.scss';
-	let { children } = $props();
+    import favicon from "$lib/assets/favicon.svg";
+    import "../app.css";
+    let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon} />
+    <link
+        rel="stylesheet"
+        href="https://raw.githubusercontent.com/sindresorhus/modern-normalize/refs/heads/main/modern-normalize.css"
+    />
 </svelte:head>
 {@render children?.()}
